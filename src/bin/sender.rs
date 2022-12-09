ef1ab56513895047ecf7f3ad8a53a65304682b25
Hidetaka Kojo <hidetaka.kojo@gmail.com>
2022-12-09 09:06:41 +0000

    print send count

--- src/bin/sender.rs
+++ src/bin/sender.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
 use clap::Parser;
-use crossbeam_channel::select;
+use crossbeam_channel::{select, tick};
 use std::net::UdpSocket;
+use std::time::Duration;
 use udp_bench::util::ctrl_channel;
 
 #[derive(Parser, Debug)]
@@ -18,9 +19,15 @@ fn main() -> Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0")?;
     socket.connect(format!("{host}:{port}", host = args.host, port = args.port))?;
 
+    let ticker = tick(Duration::from_millis(1000));
     let ctrlc_receiver = ctrl_channel()?;
+    let mut sent_count: u64 = 0;
     loop {
         select! {
+            recv(ticker) -> _ => {
+                println!("Sent {} packets", sent_count);
+                sent_count = 0;
+            }
             recv(ctrlc_receiver) -> _ => {
                 println!();
                 println!("Received Ctrl-C, exiting");
@@ -28,6 +35,7 @@ fn main() -> Result<()> {
             }
             default => {
                 let _ = socket.send(&[0, 100]);
+                sent_count += 1;
             }
         }
     }
