997df344de60d3c0f9ae0b3e24801c32f947bb8d
Hidetaka Kojo <hidetaka.kojo@gmail.com>
2022-12-09 03:07:11 +0000

    add recvmmsg-receiver

--- src/bin/recvmmsg-receiver.rs
+++ src/bin/recvmmsg-receiver.rs
@@ -1,9 +1,12 @@
 use anyhow::Result;
 use clap::Parser;
 use crossbeam_channel::{select, tick};
-use std::io;
-use std::net::UdpSocket;
-use std::time::Duration;
+use nix::errno::Errno;
+use nix::sys::socket::{
+    recvmmsg, socket, AddressFamily, MsgFlags, MultiHeaders, RecvMsg, SockFlag, SockType,
+    SockaddrIn,
+};
+use std::{io::IoSliceMut, str::FromStr, time::Duration};
 use udp_bench::util::ctrl_channel;
 
 #[derive(Parser, Debug)]
@@ -17,14 +20,31 @@ struct Args {
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let socket = UdpSocket::bind(format!("{host}:{port}", host = args.host, port = args.port))?;
-    socket.set_nonblocking(true)?;
+
+    let sock_addr = SockaddrIn::from_str(
+        format!("{host}:{port}", host = args.host, port = args.port).as_str(),
+    )?;
+
+    let socket = socket(
+        AddressFamily::Inet,
+        SockType::Datagram,
+        SockFlag::empty(),
+        None,
+    )?;
+    nix::sys::socket::bind(socket, &sock_addr)?;
 
     let ticker = tick(Duration::from_millis(1000));
     let ctrlc_receiver = ctrl_channel()?;
 
-    let mut buf = [0; 1500];
+    const BATCH_NUM: usize = 10;
+
+    let mut msgs = std::collections::LinkedList::new();
+    let mut buf = [[0u8; 1500]; BATCH_NUM];
+    msgs.extend(buf.iter_mut().map(|b| [IoSliceMut::new(&mut b[..])]));
+    let mut data = MultiHeaders::<SockaddrIn>::preallocate(msgs.len(), None);
+
     let mut received_count: u64 = 0;
+
     loop {
         select! {
             recv(ticker) -> _ => {
@@ -37,11 +57,12 @@ fn main() -> Result<()> {
                 break;
             }
             default => {
-                match socket.recv_from(&mut buf)  {
-                    Ok(_) => {
-                        received_count += 1;
+                match recvmmsg(socket, &mut data, msgs.iter(), MsgFlags::MSG_DONTWAIT, None) {
+                    Ok(res) => {
+                        let responses: Vec<RecvMsg<SockaddrIn>> = res.collect();
+                        received_count += responses.len() as u64;
                     }
-                    Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    Err(e) if (e == Errno::EAGAIN) || (e == Errno::EWOULDBLOCK) => {
                         continue;
                     }
                     Err(e) => {
